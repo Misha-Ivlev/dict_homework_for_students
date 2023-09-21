42a049d5b83e930c563fc3c409883fd05ef63eac
Misha-Ivlev <mike2001a@yandex.ru>
2023-09-21 09:35:28 +0300

    first done

--- 1_task/main.py
+++ 1_task/main.py
@@ -1,4 +1,5 @@
-"""
+"""Домашнее задание 1.
+
 Вам поручили написать простой парсер для значений, которые вы получаете из внешнего API.
 На вход вы получаете список строк, в которых нечетные слова - это ключи, а четные слова - это их значения.
 Ключи, значения которых вы должны учитывать - id, name, last_name, age, salary, position. Остальные ключи и их значения
@@ -10,11 +11,9 @@
 1) На вход подается информация по сотрудникам, где всегда четное количество слов
 2) Там, где нужны числовые значения, числа могут быть преобразованы к int или Decimal без ошибок.
 
-После парсинга вы должны получить структуру данных, описанную ниже  list[dict[str, int | str]]:
-[
+После парсинга вы должны получить структуру данных, описанную ниже  list[dict[str, int | str]]: [
     {'id': 1, 'name': 'Ivan', 'last_name': 'Ivanov', 'age': 29, 'position': developer, 'salary': Decimal('10000')},
-    ...
-]
+    ...]
 
 для тестирования запустить pytest 1_task/test.py
 """
@@ -22,14 +21,13 @@
 import os
 from decimal import Decimal
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SPLIT_SYMBOL = '\n'
 
 
 def read_file(path: str) -> str:
-    with open(path, 'r') as file:
-        file_article = file.read()
+    with open(path, 'r') as my_file:
+        file_article = my_file.read()
     return file_article
 
 
@@ -46,4 +44,20 @@ def get_parsed_employees_info() -> list[dict[str, int | str]]:
     parsed_employees_info = []
 
     # Ваш код ниже
+    for entry in employees_info:
+        parsed_employees_info.append({})
+        entry = entry.split()
+
+        for index in range(len(entry) // 2):
+            key = entry[2 * index]
+            entry_value = entry[2 * index + 1]
+
+            match key:
+                case 'id' | 'age':
+                    parsed_employees_info[-1].update({key: int(entry_value)})
+                case 'salary':
+                    parsed_employees_info[-1].update({key: Decimal(entry_value)})
+                case 'name' | 'last_name' | 'position':
+                    parsed_employees_info[-1].update({key: entry_value})
+
     return parsed_employees_info
